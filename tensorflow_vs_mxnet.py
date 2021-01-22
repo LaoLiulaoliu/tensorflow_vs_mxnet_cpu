1c76e3ec233581df6666df4bfce0b7e6ee3e7920
laoliu <q_0_0_p@163.com>
2021-01-22 15:34:59 +0800

    add

--- tensorflow_vs_mxnet.py
+++ tensorflow_vs_mxnet.py
@@ -14,65 +14,83 @@ from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
 
+
 INPUT_SIZE_STR = 'input_size_str'
+group_time = 60000
+steps = 5
+irrelevant = 2
 
 
-def downsample_filter_normalization(fname):
+def read_csv(fname):
     df = pd.read_csv(fname)
-    df['time'] = df['time'].apply(lambda x: time.strftime('%Y-%m-%d %H:%M', time.localtime(0.001 * x)))
-    df = df.groupby('time').agg(lambda x: np.mean(x)).reset_index()
-
-#    df.index = pd.to_datetime(df.pop('time'), unit='ms') + pd.Timedelta('08:00:00')
-#    base = df.index[-1]
-#    df = df.resample('60S', closed='right', label='right', base=base.second).mean()
-
-    df = df[(df['primary_air_rate'] > 0) & (df['primary_air_rate'] < 0.9)]  # in this data, no row lose
-
-    columns_without_time = df.columns.to_list()
-    columns_without_time.remove('time')
-
-    Xmin = df[columns_without_time].min(axis=0)
-    Xmax = df[columns_without_time].max(axis=0)
-    Xnorm = (df[columns_without_time] - Xmin) / (Xmax - Xmin)
-
-    return pd.concat([df['time'], Xnorm], axis=1)
+    return df[(df['primary_air_rate'] > 0) & (df['primary_air_rate'] < 0.9)]
 
 
-def build_data(df):
+def aggregation_resample(df):
+    """bias based on resample feature
+    """
+    base = df.index[-1]
+    bias = 0 if base.microsecond == 0 else 1
+    return df.resample(
+        '{}S'.format(int(group_time / 1000)),
+        closed='right',
+        label='right',
+        base=(base.second + bias) % int(group_time / 1000)
+    ).mean()
+
+
+def aggregate_time_by_min(df, add_index=True):
+    if add_index is True:
+        df.index = pd.to_datetime(df['time'], unit='ms') 
+        df.drop(columns=['time'], inplace=True)
+        return aggregation_resample(df)
+    else:
+        df['time'] = df['time'].apply(lambda x: time.strftime('%Y-%m-%d %H:%M', time.localtime(0.001 * x)))
+        return df.groupby('time').agg(lambda x: np.mean(x)).reset_index()
+
+
+def min_max_normalization(df, df_min=None, df_max=None):
+    if df_min is None:
+        df_min = df.min(axis=0)
+    if df_max is None:
+        df_max = df.max(axis=0)
+
+    df_norm = (df - df_min) / (df_max - df_min)
+
+    if 'time' in df.columns:
+        df_norm['time'] = df['time']  # pd.concat([df['time'], df_norm], axis=1)
+
+    return df_norm, df_min, df_max
+
+
+def reconstruct_train_data(df):
     """inputs按分钟聚合，临近5min数据拼一行，每行数据时间上要求是顺序相连的。如果当前时间是t，则这一行包含[t-5, t-1] 的所有数据.
        取出y，删除t-1, t-2 的y，和时间列。如果这一行有空值数据则删除此行。
     """
-    steps = 5
-    irrelevant = 2
-    data = None
+    if len(df) < steps + 1:
+        return None, None
+
+    if 'time' in df.columns:
+        convert_strtime_datetime = functools.partial(datetime.strptime)
+        time_column = list(map(lambda x: convert_strtime_datetime(x, '%Y-%m-%d %H:%M'), df['time']))
+
     labels = []
+    data = None
 
-    convert_strtime_datetime = functools.partial(datetime.strptime)
-    time_column = list(map(lambda x: convert_strtime_datetime(x, '%Y-%m-%d %H:%M'), df['time']))
-
-    for t in range(len(time_column) - 1, steps - 1, -1):
-        time_difference = timedelta(seconds=60)
+    for t in range(len(df) - 1, steps - 1, -1):
+        time_difference_seconds = int(group_time / 1000)
         item = np.empty(0)
-        for i in range(1, steps + 1):
-            if time_column[t] - time_column[t - i] != time_difference * i:
+        for j in range(1, steps + 1):
+            if (df.index[t] - df.index[t - j]).seconds != time_difference_seconds * j:
                 break
         else:
             for i in range(1, steps + 1):
-                item = np.hstack((item, df.iloc[t - i, 1:-1].values)) if i <= irrelevant else np.hstack((item, df.iloc[t - i, 1:].values))
+                item = np.hstack((item, df.iloc[t - i, :-1].values)) \
+                    if i <= irrelevant else np.hstack((item, df.iloc[t - i, :].values))
 
             data = np.vstack((data, item)) if data is not None else item
             labels.append(df.iloc[t, -1])
     return data, np.asarray(labels).reshape((-1, 1))
-
-
-def gen_lstm_data(data, step=10):
-    x, y =[], []
-    for i in range(len(data) - step):
-        x_temp = data[i:i+step, :-1]
-        y_temp = data[i+step:i+step+1, -1]
-        x.append(x_temp.tolist())
-        y.append(y_temp.tolist())
-    return np.array(x), np.array(y)
 
 
 def build_lstm_model(X, learning_rate):
@@ -130,7 +148,6 @@ class LSTMNet(mx.gluon.nn.Block):
         dense:            -> (1077, 1)
         decoder:          -> (1077, 1)
     """
-
     def __init__(self, num_hiddens, input_size):
         super(LSTMNet, self).__init__()
         self.encoder = mx.gluon.rnn.LSTM(hidden_size=num_hiddens, input_size=input_size)
@@ -144,7 +161,34 @@ class LSTMNet(mx.gluon.nn.Block):
         return outs
 
 
-def train(features, labels, num_epochs, net, trainer, loss, validation_x=None, validation_y=None):
+class LSTMNetBetter(mx.gluon.nn.Block):
+    """
+        input:     (1436, 1, 43)
+        LSTM1:     (43, 32) -> (1436, 1, 32)
+        LSTM2:     (32, 64) -> (1436, 1, 64)
+        decoder:            -> (1436, 1)
+    """
+    def __init__(self, num_hiddens, input_size):
+        """
+        :param num_hiddens: 64 is optimized in some algorighm
+        :param input_size:
+        """
+        super(LSTMNetBetter, self).__init__()
+        half_hiddens = int(num_hiddens * 0.5)
+        self.encoder = mx.gluon.rnn.LSTM(hidden_size=half_hiddens, input_size=input_size)
+        self.drop = mx.gluon.nn.Dropout(0.01)
+        self.encoder2 = mx.gluon.rnn.LSTM(hidden_size=num_hiddens, input_size=half_hiddens)
+        self.drop2 = mx.gluon.nn.Dropout(0.01)
+        self.decoder = mx.gluon.nn.Dense(1)
+
+    def forward(self, inputs):
+        outputs1 = self.drop(self.encoder(inputs))
+        outputs = self.drop2(self.encoder2(outputs1))
+        outs = self.decoder(outputs)
+        return outs
+
+
+def training_procedure(features, labels, net, num_epochs, trainer, loss, validation_x=None, validation_y=None):
     start = time.time()
     for epoch in range(num_epochs):
         train_l_sum, n = 0.0, 0
@@ -182,7 +226,7 @@ def load_mxnet_model(fname):
         model = pickle.load(fd)
     input_size = model.pop(INPUT_SIZE_STR)
 
-    net = LSTMNet(64, input_size)
+    net = LSTMNetBetter(64, input_size)
     params = net._collect_params_with_prefix()
     for name in model:
         if name in params:
@@ -193,20 +237,23 @@ def load_mxnet_model(fname):
 
 def run_mxnet(train_x, train_y, num_epochs, learning_rate, validation_x=None, validation_y=None):
     input_size = train_x.shape[2]
-    net = LSTMNet(64, input_size)
+    net = LSTMNetBetter(64, input_size)
     net.initialize(mx.init.Xavier())
     net.hybridize()
+
     trainer = mx.gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': learning_rate})
-    loss = mx.gluon.loss.L2Loss()
-    train(mx.nd.array(train_x), mx.nd.array(train_y), num_epochs, net, trainer, loss, mx.nd.array(validation_x),
-          mx.nd.array(validation_y))
+    training_procedure(mx.nd.array(train_x), mx.nd.array(train_y), net,
+        num_epochs, trainer,
+        mx.gluon.loss.L2Loss(),
+        mx.nd.array(validation_x),
+        mx.nd.array(validation_y))
     save_mxnet_model(net, './mxnet.net', input_size) # net.save_parameters('./mxnet.net')
 
 
 def load_mxnet(X, Y):
     start = time.time()
     net = load_mxnet_model('./mxnet.net')
-    # net = LSTMNet(64, X.shape[2])
+    # net = LSTMNetBetter(64, X.shape[2])
     # net.load_parameters('./mxnet.net')
     net.hybridize()
 
@@ -226,17 +273,19 @@ def linear_model_way(train_x, train_y):
 
 
 def main():
-    df = downsample_filter_normalization('datas.csv')
-    data, Y = build_data(df)
-    print(df.shape, data.shape, Y.shape)  # (1441, 10) (1436, 43) (1436, 1)
-    data = data.astype('float32')
-    # Y = Y.astype('float32')
+    df = read_csv('datas.csv')
+    df = aggregate_time_by_min(df, True)
+    df_norm, df_min, df_max = min_max_normalization(df)
+    datas, labels = reconstruct_train_data(df_norm)
 
-    validation_factor = int(0.75 * data.shape[0])
-    train_x = data[:validation_factor, :]
-    train_y = Y[:validation_factor]
-    validation_x = data[validation_factor:, :]
-    validation_y = Y[validation_factor:]
+    print(df_norm.shape, datas.shape, labels.shape)  # (1441, 9) (1436, 43) (1436, 1)
+    datas = datas.astype('float32')
+
+    validation_factor = int(0.8 * datas.shape[0])
+    train_x = datas[:validation_factor, :]
+    train_y = labels[:validation_factor]
+    validation_x = datas[validation_factor:, :]
+    validation_y = labels[validation_factor:]
 
     linear_model_way(train_x, train_y)
 
@@ -269,7 +318,7 @@ def main():
     # whole time: 1.1297211647033691
     s = time.time()
     learning_rate = 0.002
-    num_epochs = 300
+    num_epochs = 500
     run_mxnet(train_x, train_y, num_epochs, learning_rate, validation_x, validation_y)
     load_mxnet(validation_x, validation_y)
 
